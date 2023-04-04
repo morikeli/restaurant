d664ff6c96cbd5cf8090d4d77276cd1a06925345
brocode <kelimbth@gmail.com>
2023-04-04 15:18:26 +0300

    added new db table fields

--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,12 @@ class User(AbstractUser):
     id = models.CharField(max_length=20, primary_key=True, unique=True, editable=False)
     email = models.EmailField(unique=True)
     gender = models.CharField(max_length=7, blank=False)
+    age = models.PositiveIntegerField(default=0, editable=False)
+    dob = models.DateField(blank=False, null=False)
+    national_id = models.CharField(max_length=8, blank=False)
     phone_no = models.CharField(max_length=10, blank=False)
-    updated = models.DateTimeField(auto_now=True)
     role = models.CharField(max_length=10, blank=False)
+    updated = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
